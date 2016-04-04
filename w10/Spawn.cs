c9585b6ce963a68906654cab32eb812fab3dd74b
snam4 <snam4@saic.edu>
2016-04-04 16:40:40 -0400

    W10
    Coin Random Spawn

---
+++ w10/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+	public GameObject coin;
+	public float timer = 0.0f;
+	private Vector3 spawn_position; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Debug.Log (timer);
+		timer = timer + Time.deltaTime;
+		if (timer > 10) {
+			timer = 0.0f;
+			SpawnCoin ();
+		}
+	}
+
+	void SpawnCoin(){
+		spawn_position = new Vector3(Random.Range(0F, 20.0F), 0.5f, Random.Range(0F, 20.0F));
+
+		Instantiate (coin, spawn_position, Quaternion.Euler (new Vector3 (0, 0, 90)));
+
+	}
+}
